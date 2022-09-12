c50e87c273afb97ed1645f2f7c6d01b2ccf5d05a
linux_china <libing.chen@gmail.com>
2022-09-12 12:51:27 +0800

    test: HttpBinClientTest

---
+++ src/test/java/org/mvnsearch/httpinterface/HttpBinClientTest.java
@@ -0,0 +1,25 @@
+package org.mvnsearch.httpinterface;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+public class HttpBinClientTest {
+    private static HttpBinClient httpBinClient;
+
+    @BeforeAll
+    public static void setUp() throws Exception {
+        WebClient webClient = WebClient.builder().build();
+        HttpServiceProxyFactory httpServiceProxyFactory = new HttpServiceProxyFactory(WebClientAdapter.forClient(webClient));
+        httpServiceProxyFactory.afterPropertiesSet();
+        httpBinClient = httpServiceProxyFactory.createClient(HttpBinClient.class);
+    }
+
+    @Test
+    public void testClient() {
+        System.out.println(httpBinClient.myIp().origin());
+        System.out.println(httpBinClient.post("Hello").data());
+    }
+}
